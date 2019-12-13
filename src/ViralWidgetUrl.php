c97321614abdda1204e5f2f3d1898b6f51d4f302
Bartosz Mikolajczak <b.mikolajczak@booklet.pl>
2019-12-13 10:09:34 +0100

    chore(url): fix setup url variable in recommendation code remover

--- src/ViralWidgetUrl.php
+++ src/ViralWidgetUrl.php
@@ -3,11 +3,12 @@ class ViralWidgetUrl
 {
     public function createCurrentUrlWitoutRecommendationCodeParam(string $url = null, $in_params = [])
     {
+        $url = $url ?? $_SERVER['REQUEST_URI'];
+
         // for recommendation code passed as part of url (match)
         // https://fotobum.pl/zabawa-polecaj/eavtmu => https://fotobum.pl/zabawa-polecaj
         $recommendation_code = $in_params[ViralWidget::RECOMMENDATION_KEY] ?? null;
         if ($recommendation_code and !$this->urlHaveRecommendationCodeParam($url)) {
-            $url = $url ?? $_SERVER['REQUEST_URI'];
             $url = str_replace('/' . $recommendation_code, '' ,$url);
 
             return $url;
@@ -15,7 +16,6 @@ class ViralWidgetUrl
 
         // for recommendation code passed in get params
         // http://fotobum.pl/viral?recommendation_code=22ehdb =>'http://fotobum.pl/viral
-        $url = $url ?? $_SERVER['REQUEST_URI'];
         $parsed_url = parse_url($url);
 
         if (empty($parsed_url['query'])) {
